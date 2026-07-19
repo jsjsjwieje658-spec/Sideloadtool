1aa26d4df83b7f1cdf12adc1e2147a848d3368e9
jsjsjwieje658-spec <jsjsjwieje658@gmail.com>
2026-07-19 14:50:18 +0700

    Update usb_fd_bridge.c

--- app/src/main/cpp/usb_fd_bridge.c
+++ app/src/main/cpp/usb_fd_bridge.c
@@ -54,6 +54,7 @@
 
 #define TAG "usb_fd_bridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,  TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
 /* Apple AMDI interface */
